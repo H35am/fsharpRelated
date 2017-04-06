05fb80bc0f6ccdbe3560ff8b5a343cb55a03c22a
HeszaR <heszar@gmail.com>
2017-04-07 01:01:22 +0200

    quad

---
+++ FirstProgram/App1/Model.fs
@@ -0,0 +1,34 @@
+﻿module Model
+
+type Details =
+    {Name: string
+     Description: string}
+
+type Item =
+    { Details: Details}
+ 
+ // descriminated unions
+ type Exit =
+    | PassableExit of Details * destination: Room
+    | LockedExit of Details  * key: Item * next: Exit 
+    | NoExit of Details option
+    
+and Exits =
+    {North: Exit
+     South: Exit
+     West: Exit
+     East: Exit }
+
+and Room =
+    { Details: Details
+      Items: Item list
+      Exits: Exits }
+
+let firstRoom =
+    { Details = {Name = "First Room"; Description = "You're standing in a room!"};
+      Items = [];
+      Exits = {North = NoExit(None);
+               South = NoExit(None); 
+               East = NoExit(None);
+               West = NoExit(None);}
+      }
---
+++ README.md
@@ -0,0 +1,9 @@
+Fsharp related stuff:
+
+FirstProgram folder contains training programs related to "Expert F# 2.0" book.
+
+SchoolOpdracht1 folder contains training programs related to course.
+
+Tutorial1 folder contains first tutorial provided by Visual Studio 2017.
+
+SVM folder (Simplified Virtual Machine) contains final assignment for this course.
